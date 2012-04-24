9e09b807dcc5cd6f8cd7d6bd5deccb367cbec649
Dmitry Deshevoy <mityada@gmail.com>
2012-04-25 01:51:44 +0400

    _draw_char

--- src/dcpu.asm
+++ src/dcpu.asm
@@ -1,12 +1,33 @@
 section .text
 	extern _create_window
 	extern _process_events
+	extern _draw_char
 
 	global _start
 
 _start:
 	call _create_window
 
+	mov ecx, 12
+_draw:
+	push ecx
+
+	push 0xf00ff00f
+	push ecx
+	push ecx
+	call _draw_char
+	add esp, 12
+
+	pop ecx
+	loop _draw
+
 _loop:
 	call _process_events
+
+	push 0xffffffff
+	push 0
+	push 0
+	call _draw_char
+	add esp, 12
+
 	jmp _loop
--- src/display.asm
+++ src/display.asm
@@ -1,10 +1,11 @@
-%define DCPU_PIXEL_SIZE 5
+%define DCPU_PIXEL_SIZE 4
 
 section .bss
 	display resd 1
 	screen	resd 1
 	window	resd 1
 	event	resd 24
+	gc	resd 1
 
 section .text
 	extern XOpenDisplay
@@ -12,9 +13,14 @@ section .text
 	extern XSelectInput
 	extern XMapWindow
 	extern XNextEvent
+	extern XCreateGC
+	extern XSetForeground
+	extern XFillRectangle
+	extern XFlush
 
 	global _create_window
 	global _process_events
+	global _draw_char
 
 _create_window:
 	push 0
@@ -57,13 +63,103 @@ _create_window:
 	call XMapWindow
 	add esp, 8
 
+	push 0				; values
+	push 0				; valuemask
+	mov eax, [window]
+	push eax			; d
+	mov eax, [display]
+	push eax			; display
+	call XCreateGC
+	add esp, 16
+	mov [gc], eax
+
+_wait_map_notify:
+	call _process_events
+	cmp dword [event], 19
+	jne _wait_map_notify
+
 	ret
 
 _process_events:
-	push event		; event_return
+	push event			; event_return
 	mov eax, [display]
-	push eax		; display
+	push eax			; display
 	call XNextEvent
 	add esp, 8
 
+	ret
+
+_draw_char:
+	push ebx
+	push esi
+	push edi
+
+	mov eax, [screen]
+	mov eax, [eax + 13 * 4]		; black_pixel
+	push eax			; foreground
+	mov eax, [gc]
+	push eax			; gc
+	mov eax, [display]
+	push eax			; display
+	call XSetForeground
+	add esp, 12
+
+	mov esi, [esp + 16]		; x
+	mov edi, [esp + 20]		; y
+	mov ebx, [esp + 24]		; char
+
+	lea esi, [esi * 4]
+	lea edi, [edi * 8]
+	lea esi, [esi * DCPU_PIXEL_SIZE]
+	lea edi, [edi * DCPU_PIXEL_SIZE]
+
+	push 0
+	push 7
+
+	push DCPU_PIXEL_SIZE		; height
+	push DCPU_PIXEL_SIZE		; width
+	push 0				; y
+	push 0				; x
+	mov eax, [gc]
+	push eax			; gc
+	mov eax, [window]
+	push eax			; d
+	mov eax, [display]
+	push eax			; display
+
+_loop_height:
+	mov dword [esp + 28], 7
+_loop_width:
+	test ebx, 1 << 31
+	jz _continue
+
+	mov ecx, [esp + 32]
+	lea eax, [esi + ecx * DCPU_PIXEL_SIZE]
+	mov [esp + 12], eax
+	mov edx, [esp + 28]
+	lea eax, [edi + edx * DCPU_PIXEL_SIZE]
+	mov [esp + 16], eax
+	call XFillRectangle
+
+_continue:
+	shl ebx, 1
+
+	sub dword [esp + 28], 1
+	jge _loop_width
+	add dword [esp + 32], 1
+	cmp dword [esp + 32], 4
+	jne _loop_height
+
+	add esp, 7 * 4
+
+	add esp, 8
+
+	mov eax, [display]
+	push eax
+	call XFlush
+	add esp, 4
+
+	pop edi
+	pop esi
+	pop ebx
 	ret
