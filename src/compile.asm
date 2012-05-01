d0c7f4d2e4ddb6e7cfff12a2906a9cb6c0994d0c
Korobochka <ilya-korobitsyn@narod.ru>
2012-05-01 13:10:40 +0400

    Compiler: some cosmetical changes

--- src/compile.asm
+++ src/compile.asm
@@ -8,13 +8,18 @@ section .data
 	file_name      dd 0
 	buff           dd 0
 	buff_size      dd 0
-	buff2          dd 0
+	buff2          dd 0 ; pointer to temporary buffer
 	buff2_size     dd 0
-	curr           dd 0
-	symbols        db ";:[],+-_"
+	curr           dd 0 ; used by append
+	symbols        db ";:[],+-_" ; additional symbols allowed in the input file
 	symbols_cnt    dd 8
 	usage          db "Usage: compile file.dasm", 10, 0
 	no_file        db "Unable to open file", 10, 0
+	
+	positions      dd 0 ; pointer to array with token positions in buff
+	types          dd 0 ; pointer to array with token types
+	sizes          dd 0 ; pointer to array with token resulting sizes
+	places         dd 0 ; pointer to array with token resulting places
 
 section .text
 	extern exit
@@ -33,17 +38,34 @@ section .text
 ;-------------------------------------
 _start:
 
-	pop esi
-	cmp esi, 2
+	cmp dword [esp], 2
 	jne _usage
 	
-	pop esi
-	pop dword [file_name]
+	mov esi, dword[esp + 8]
+	mov dword [file_name], esi
 	
 	push dword [file_name]
 	call _printf_s
 	add esp, 4
 
+
+	call _read_file
+	call _remove_comments
+	call _print_buff
+	
+	push dword [buff]
+	call _strlen
+	mov [esp], eax
+	call _printf_i
+	add esp, 4
+	push dword [buff_size]
+	call _printf_i
+	add esp, 4
+
+	jmp _exit
+
+;-------------------------------------
+_read_file:
 	push fopen_mode
 	push dword [file_name]
 	call fopen
@@ -92,20 +114,7 @@ _start:
 	call fclose
 	add esp, 4
 	
-	call _remove_comments
-	
-	mov ebp, 0
-.out_loop:
-	mov edx, dword [buff]
-	movzx eax, byte [edx + ebp]
-	push eax
-	call _printf_c
-	add esp, 4
-	inc ebp
-	cmp ebp, dword [buff_size]
-	jb .out_loop
-	
-	jmp _exit
+	ret
 	
 ;-------------------------------------
 _remove_comments:
@@ -172,20 +181,32 @@ _remove_comments:
 	
 	
 .end:
+	push -1
+	call _append
+	add esp, 4
+
 	push dword [buff]
 	call free
 	add esp, 4
 	mov eax, dword [buff2]
 	mov dword [buff], eax
 	mov ecx, dword [curr]
+	dec ecx
 	mov dword [buff_size], ecx
 
 	ret
-	
-_append:
+
+;-------------------------------------	
+_append: ; will append to buff2 symbol buff[arg1], or 0 if arg1 == -1
 	mov eax, [esp + 4]
 	mov edx, dword [buff]
+	cmp eax, 0
+	jb .null
 	movzx ecx, byte [edx + eax]
+	jmp .not_null
+.null:
+	xor ecx, ecx
+.not_null:
 	mov eax, dword [curr]
 	mov edx, dword [buff2]
 	mov byte [edx + eax], cl
@@ -193,11 +214,11 @@ _append:
 	ret
 	
 ;-------------------------------------
-_make_list:
+_tokenize: ; parse buff into tokens
 	
 	
 ;-------------------------------------
-_test_symbol:
+_test_symbol: ; will check if arg1 is valid symbol
 	mov ecx, [esp + 4]
 	lea edx, [ecx - '0']
 	cmp edx, '9' - '0'
@@ -227,6 +248,21 @@ _test_symbol:
 	ret
 .ok:
 	mov eax, 1
+	ret
+
+;-------------------------------------
+_print_buff:
+	mov ebp, 0
+.out_loop:
+	mov edx, dword [buff]
+	movzx eax, byte [edx + ebp]
+	push eax
+	call _printf_c
+	add esp, 4
+	inc ebp
+	cmp ebp, dword [buff_size]
+	jb .out_loop
+	
 	ret
 
 ;-------------------------------------
